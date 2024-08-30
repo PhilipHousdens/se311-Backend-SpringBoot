c0814c51cf94615b6eb88d6967bb80d2b8ee8367
Philip Housden <philip.houden.04@gmail.com>
2024-08-30 11:13:22 +0700

    finished lab07

---
+++ src/main/java/se331/lab/dao/OrganizerDaoImpl.java
@@ -0,0 +1,71 @@
+package se331.lab.dao;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+import se331.lab.entity.Organizer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class OrganizerDaoImpl implements OrganizerDao {
+    List<Organizer> organizers;
+
+
+    @PostConstruct
+    public void init() {
+        organizers = new ArrayList<>();
+
+        organizers.add(Organizer.builder()
+                .id(1L)
+                .name("Kat Laydee")
+                .address("Meow Town")
+                .build());
+
+        organizers.add(Organizer.builder()
+                .id(2L)
+                .name("Fern Pollin")
+                .address("Flora City")
+                .build());
+
+        organizers.add(Organizer.builder()
+                .id(3L)
+                .name("Carey Wales")
+                .address("Playa Del Carmen")
+                .build());
+
+        organizers.add(Organizer.builder()
+                .id(4L)
+                .name("Dawg Dahd")
+                .address("Woof Town")
+                .build());
+
+        organizers.add(Organizer.builder()
+                .id(5L)
+                .name("Kahn Opiner")
+                .address("Tin City")
+                .build());
+
+        organizers.add(Organizer.builder()
+                .id(6L)
+                .name("Brody Kill")
+                .address("Highway 50")
+                .build());
+    }
+
+    @Override
+    public Integer getOrganizerSize() {return organizers.size();}
+
+    @Override
+    public List<Organizer> getOrganizers(Integer pageSize, Integer page) {
+        pageSize = pageSize == null ? organizers.size() : pageSize;
+        page = page == null ? 1 : page;
+        int firstIndex = (page - 1) * pageSize;
+        return organizers.subList(firstIndex, firstIndex + pageSize);
+    }
+
+    @Override
+    public Organizer getOrganizer(Long id) {
+        return organizers.stream().filter(o -> o.getId().equals(id)).findFirst().orElse(null);
+    }
+}
--- src/main/java/se331/lab/entity/Organizer.java
+++ src/main/java/se331/lab/entity/Organizer.java
@@ -6,7 +6,7 @@ import lombok.Data;
 @Data
 @Builder
 public class Organizer {
-    private int id;
-    private String name;
-    private String address;
+    Long id;
+    String name;
+    String address;
 }
