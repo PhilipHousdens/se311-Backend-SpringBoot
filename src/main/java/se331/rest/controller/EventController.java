6d96ab8f8ccabf14cdf26c42e9b8506dc4c6d3c0
Philip Housden <philip.houden.04@gmail.com>
2024-09-14 13:03:34 +0700

    finished question 5

--- src/main/java/se331/rest/controller/EventController.java
+++ src/main/java/se331/rest/controller/EventController.java
@@ -2,13 +2,12 @@ package se331.rest.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import se331.rest.entity.Event;
 import se331.rest.service.EventService;
@@ -21,16 +20,10 @@ public class EventController {
     final EventService eventService;
     @GetMapping("events")
     public ResponseEntity<List<Event>> getEventLists(@RequestParam(value = "_limit", required = false) Integer perPage, @RequestParam(value = "_page", required = false) Integer page) {
-        List<Event> output = null;
-        Integer eventSize = eventService.getEventSize();
+        Page<Event> pageOutput = eventService.getEvents(perPage, page);
         HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.set("x-total-count", String.valueOf(eventSize));
-        try {
-            output = eventService.getEvents(perPage, page);
-        } catch (IndexOutOfBoundsException ex) {
-            return new ResponseEntity<>(output, responseHeaders, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(output, responseHeaders, HttpStatus.OK);
+        responseHeaders.set("x-total-count", String.valueOf(pageOutput.getTotalElements()));
+        return new ResponseEntity<>(pageOutput.getContent(), responseHeaders, HttpStatus.OK);
     }
 
     @GetMapping("events/{id}")
@@ -41,6 +34,12 @@ public class EventController {
         }else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND,"The given id is not found");
         }
+    }
+
+    @PostMapping("/events")
+    public ResponseEntity<?> addEvent(@RequestBody Event event) {
+        Event output = eventService.save(event);
+        return ResponseEntity.ok(output);
     }
 }
 
