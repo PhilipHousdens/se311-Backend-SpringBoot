00e2ea2b28142ff74e1406a23ca7540b98c3269a
Philip Housden <philip.houden.04@gmail.com>
2024-09-29 19:14:12 +0700

    until question6.2

--- src/main/java/se331/rest/dao/OrganizerDao.java
+++ src/main/java/se331/rest/dao/OrganizerDao.java
@@ -1,12 +1,2 @@
-package se331.rest.dao;
-
-import org.springframework.data.domain.Page;
-import se331.rest.entity.Organizer;
-
-
-public interface OrganizerDao {
-    Integer getOrganizerSize();
-    Page<Organizer> getOrganizers(Integer pageSize, Integer page);
-    Organizer getOrganizer(Long id);
-    Organizer save(Organizer organizer);
+package se331.rest.dao;public interface OrganizerDao {
 }
--- src/main/java/se331/rest/service/OrganizerService.java
+++ src/main/java/se331/rest/service/OrganizerService.java
@@ -1,13 +1,2 @@
-package se331.rest.service;
-
-import org.springframework.data.domain.Page;
-import se331.rest.entity.Organizer;
-
-import java.util.List;
-
-public interface OrganizerService {
-    Integer getOrganizerSize();
-    Page<Organizer> getOrganizers(Integer pageSize, Integer page);
-    Organizer getOrganizer(Long id);
-    Organizer save(Organizer organizer);
+package se331.rest.service;public interface OrganizerService {
 }
